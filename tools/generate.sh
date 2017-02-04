d8338303299c63e9367bf972e70817ee8e6acf73
Josh Mandel <jmandel@med.harvard.edu>
2017-02-04 07:37:06 -0500

    Keep certs dir
    Reorg
    
    Reorg

--- tools/generate.sh
+++ tools/generate.sh
@@ -1,10 +1,9 @@
-docker build src -t certs-from-rackspace
+docker build ./tools/generate -t certs-from-rackspace
 source  ~/Private/rackspace/environment
 docker run --rm -it \
   -e DOMAINS="pilot-secure-demo-app.syncfor.science pilot-secure-test-suite.syncfor.science" \
   -e USERNAME="$OS_USERNAME" -e API_KEY="$OS_API_KEY" \
-  -v $(readlink -f certs):/dehydrated/certs \
+  -v $(readlink -f config/proxy/certs):/dehydrated/certs \
   certs-from-rackspace
 
-sudo chown -R $USER.$USER certs
-
+sudo chown -R $USER.$USER config/proxy/certs
