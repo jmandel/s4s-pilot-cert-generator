7db52d461b385981c3931bd7abba2fe2b9c5d609
J M <jmandel@test-jenkins>
2017-04-23 10:24:44 -0400

    Fix logging of secret export

--- tools/secret-export.sh
+++ tools/secret-export.sh
@@ -5,4 +5,4 @@ tar -czvf ../secret.tgz ../secret
 gpg --encrypt -r pilot-prep@syncfor.science ../secret.tgz
 rm ../secret.tgz
 mv ../secret.tgz.gpg ~/Dropbox/S4S/
-echo "Created ~/Dropbox/S4S/secret.tgz"
+echo "Created ~/Dropbox/S4S/secret.tgz.gpg"
